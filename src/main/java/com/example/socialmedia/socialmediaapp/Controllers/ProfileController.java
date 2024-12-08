7d884ad1bf86b11709b0c72ef163044a564d728f
Inderneel08 <68139492+Inderneel08@users.noreply.github.com>
2024-12-08 10:46:39 +0530

    12/08/2024

--- src/main/java/com/example/socialmedia/socialmediaapp/Controllers/ProfileController.java
+++ src/main/java/com/example/socialmedia/socialmediaapp/Controllers/ProfileController.java
@@ -1,10 +1,12 @@
 package com.example.socialmedia.socialmediaapp.Controllers;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.example.socialmedia.socialmediaapp.Service.CustomUserDetails;
 
@@ -35,4 +37,12 @@ public class ProfileController {
 
         return ("profile");
     }
+
+
+    @PostMapping("/update-profile")
+    public ResponseEntity<?> updateProfile()
+    {
+        return null;
+    }
+
 }
