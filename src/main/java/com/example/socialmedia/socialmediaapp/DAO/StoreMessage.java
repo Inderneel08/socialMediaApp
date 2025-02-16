f8107b8e29bd5cd23e65e1b7dacd917c35afebaf
Inderneel08 <68139492+Inderneel08@users.noreply.github.com>
2025-02-16 19:27:33 +0530

    02/16/2025

---
+++ src/main/java/com/example/socialmedia/socialmediaapp/DAO/StoreMessage.java
@@ -0,0 +1,44 @@
+package com.example.socialmedia.socialmediaapp.DAO;
+
+import java.math.BigInteger;
+import java.sql.Timestamp;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "messages")
+@Getter
+@Setter
+public class StoreMessage {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private BigInteger id;
+
+    @Column(name = "senderId")
+    private BigInteger senderId;
+
+    @Column(name = "recieverId")
+    private BigInteger recieverId;
+
+    @Column(name = "messageSend")
+    private String messageSend;
+
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private Timestamp created_at;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at", nullable = false)
+    private Timestamp updated_at;
+}
